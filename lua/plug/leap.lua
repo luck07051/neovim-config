8d68055596a3563e4299f1831440e30f0ee931cd
luck07051 <luck0751@gmail.com>
2023-09-30 14:09:20 +0800

    add dependencies

--- lua/plug/leap.lua
+++ lua/plug/leap.lua
@@ -1,6 +1,10 @@
 return {
   {
     'ggandor/leap.nvim',
+    dependencies = {
+      'ggandor/leap.nvim',
+      'tpope/vim-repeat',
+    },
     keys = {
       {
         "s",
@@ -66,6 +70,7 @@ return {
     'ggandor/flit.nvim',
     dependencies = {
       'ggandor/leap.nvim',
+      'tpope/vim-repeat',
     },
     opts = {},
     keys = {
