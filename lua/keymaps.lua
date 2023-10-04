bc4fd3824557ad280be02956da149f044dcd704a
luck07051 <luck0751@gmail.com>
2023-10-05 03:14:05 +0800

    map ; to /

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -18,6 +18,9 @@ vim.keymap.set('t', '<C-[>', '<C-\\><C-n>')
 vim.keymap.set('t', '<C-w>', '<C-\\><C-n><C-w>')
 
 
+-- Spacial map just for my custom keyboard
+vim.keymap.set('n', ';', '/')
+
 -- Buffer movement
 vim.keymap.set('n', '<BS>', '<C-^>')
 vim.keymap.set('n', '<Tab>', ':bn<cr>')
