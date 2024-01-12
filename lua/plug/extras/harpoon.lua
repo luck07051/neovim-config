ece504c7c56c0720b6c4a624e3ef5c9d6560724a
luck07051 <luck0751@gmail.com>
2024-01-13 04:25:11 +0800

    change harpoon keymap form backspace

--- lua/plug/extras/harpoon.lua
+++ lua/plug/extras/harpoon.lua
@@ -8,8 +8,8 @@ return {
         local harpoon = require("harpoon")
         harpoon:setup()
 
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
-        vim.keymap.set("n", "<BS>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        vim.keymap.set("n", "<Leader>h", function() harpoon:list():append() end)
+        vim.keymap.set("n", "<C-h>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
         --
         -- vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
         -- vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
