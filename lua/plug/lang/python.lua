4a3b37e461fdd387d32c587a0221648512786620
luck07051 <luck0751@gmail.com>
2024-01-13 17:48:09 +0800

    add more lang

--- lua/plug/lang/python.lua
+++ lua/plug/lang/python.lua
@@ -1,5 +1,12 @@
 return {
   {
+    'williamboman/mason.nvim',
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { 'python-lsp-server' })
+    end,
+  },
+
+  {
     'neovim/nvim-lspconfig',
     opts = {
       servers = {
